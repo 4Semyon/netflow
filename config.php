716be1d39a43ea620b2a23c0e69f1d87371f3357
4Semyon <4Semyon@users.noreply.github.com>
2016-09-13 13:04:15 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@ define('SYSLOG_IP','10.0.4.177');
 define('SYSLOG_PORT','514');
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'Pu$hka01');
+define('DB_USER', 'login');
+define('DB_PASS', 'Password');
 define('DB_NAME', 'netflows');
 define('TABLE_NAME', 'nfcapd');
 
